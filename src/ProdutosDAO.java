5d2d2b547a6c82dbbbcb5c40d7bfe032c5572ca1
Carolina Friedrich Lobo D Avila <137728605+carolinafld@users.noreply.github.com>
2024-05-24 15:50:19 -0400

    Funcionalidade salvar no BD ao cadastrar um novo produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 /**
  *
- * @author Adm
+ * @author Carolina
  */
 
 import java.sql.PreparedStatement;
@@ -13,7 +8,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
     
@@ -23,9 +18,21 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        conn = new conectaDAO().connectDB();
         
-        
-        //conn = new conectaDAO().connectDB();
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)");
+            
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            
+            // Mensagem de sucesso
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + ex.getMessage());
+        }
         
         
     }
--- src/cadastroVIEW.java
+++ src/cadastroVIEW.java
@@ -1,11 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 
 /**
  *
- * @author Adm
+ * @author Carolina
  */
 public class cadastroVIEW extends javax.swing.JFrame {
 
